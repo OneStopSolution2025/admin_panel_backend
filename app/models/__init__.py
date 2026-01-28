7738d77205f3119144cb3eda600dfb282ec93474
One Stop Solutions <onestopsolutions.creative@gmail.com>
2026-01-28 15:35:40 +0530

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Models Package
-from models.models import User, Wallet, Transaction, UserActivity, RefreshToken, UserType, TransactionType, TransactionPurpose, Base
-from models.template_models import Template, TemplateField, TemplateFieldType
-from models.ticket_models import Ticket, TicketComment, TicketAttachment, TicketStatus, TicketPriority
-
-__all__ = ["User", "UserType", "UserActivity", "RefreshToken", "Wallet", "Transaction", "TransactionType", "TransactionPurpose", "Template", "TemplateField", "TemplateFieldType", "Ticket", "TicketComment", "TicketAttachment", "TicketStatus", "TicketPriority", "Base"]
